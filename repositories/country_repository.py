fb7a4d7831f18f74d0c416e6a806519446b9ec6e
Emma Hack <ehack78@gmail.com>
2020-11-07 16:20:58 +0000

    country_repository started

---
+++ repositories/country_repository.py
@@ -0,0 +1,35 @@
+from db.run_sql import run_sql
+
+from models.country import Country
+
+def save(country):
+    sql = "INSERT INTO countries(name, continent) VALUES ( %s, %s ) RETURNING id"
+    values = (country.name, country.continent)
+    results = run_sql(sql, values)
+    country.id = results[0]['id']
+    return country
+
+def select_all():
+    countries = []
+# when I list countries I want them to be alphabetical order each time
+    sql = "SELECT * FROM countries ORDER BY name ASC"
+    results = run_sql(sql)
+
+    for row in results:
+        country = Country(row['name'], row['continent'], row['id'])
+        countries.append(country)
+    return countries
+
+def select(id):
+    country = None
+    sql = "SELECT * FROM countries WHERE id = %s"
+    values = [id]
+    result = run_sql(sql, values)[0]
+
+    if result is not None:
+        country = Country(result['name'], result['continent'], result['id'])
+    return country
+
+def delete_all():
+    sql = "DELETE FROM countries"
+    run_sql(sql)
