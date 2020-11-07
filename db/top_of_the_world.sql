79caa376b3478476695f9ed433a13a79071da652
Emma Hack <ehack78@gmail.com>
2020-11-07 12:57:52 +0000

    database tables set up

---
+++ db/top_of_the_world.sql
@@ -0,0 +1,24 @@
+DROP TABLE attractions;
+DROP TABLE countries;
+DROP TABLE cities;
+
+CREATE TABLE countries (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255),
+    continent VARCHAR(255)
+);
+
+CREATE TABLE cities (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255),
+    country_id INT REFERENCES countries(id) ON DELETE CASCADE,
+    visited BOOLEAN,
+    notes TEXT
+);
+
+CREATE TABLE attractions (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255),
+    cost FLOAT,
+    city_id INT REFERENCES cities(id) ON DELETE CASCADE
+);
