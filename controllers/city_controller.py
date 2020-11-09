82d772739de6414d70c7e31143bc8f57f98b6af4
Emma Hack <ehack78@gmail.com>
2020-11-09 12:49:09 +0000

    add a city method is now working

--- controllers/city_controller.py
+++ controllers/city_controller.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, redirect
 from flask import Blueprint
 from models.city import City
 import repositories.city_repository as city_repository
+import repositories.country_repository as country_repository
 
 cities_blueprint = Blueprint("cities", __name__)
 
@@ -23,10 +24,22 @@ def show_city(id):
 @cities_blueprint.route("/cities/new", methods=["GET"])
 def new_city():
     cities = city_repository.select_all()
-    return render_template("cities/new.html", cities = cities)
+    countries = country_repository.select_all()
+    return render_template("cities/new.html", cities = cities, countries = countries)
 
 # CREATE
 
+@cities_blueprint.route("/cities", methods=["POST"])
+def create_city():
+    name = request.form["name"]
+    country_id = request.form['country']
+    notes = request.form["notes"]
+    visited = request.form["visited"]
+
+    country = country_repository.select(country_id)
+    city = City(name, country, notes, visited)
+    city_repository.save(city)
+    return redirect("/cities")
 
 # EDIT
 
